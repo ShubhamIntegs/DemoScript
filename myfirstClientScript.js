48ac46cae8975cd40e9b0b5c77226319472252a9
ShubhamIntegs <47977133+ShubhamIntegs@users.noreply.github.com>
2019-02-26 11:39:08 +0530

    Update myfirstClientScript.js

--- myfirstClientScript.js
+++ myfirstClientScript.js
@@ -1,3 +1,15 @@
 function firstScript(){
  alert ("Hello world");
 }
+function SaveFunc(){
+ alert ("Hello world");
+}
+function ResetFunc(){
+ alert ("Hello world");
+}
+function ReadFunc(){
+ alert ("Hello world");
+}
+function ClientScript(){
+ alert ("Hello world");
+}
